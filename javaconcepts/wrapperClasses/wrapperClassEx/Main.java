ad20b3e63b3b667eba22da9c28969d0bd26d6293
Vamsi Krishna <20816971+petlurivamsi@users.noreply.github.com>
2023-10-07 21:20:14 +0530

    wrapperClasses ex

---
+++ javaconcepts/wrapperClasses/wrapperClassEx/Main.java
@@ -0,0 +1,17 @@
+package javaconcepts.wrapperClasses.wrapperClassEx;
+
+public class Main {
+    public static void main(String[] args) {
+        Boolean b = true;
+        Integer i = 432;
+        Double a = 3.44;
+
+        if(b==true){
+            System.out.println("b is true");
+        }
+        if(i==432){
+            System.out.println("i value is "+i);
+        }
+    }
+    
+}
