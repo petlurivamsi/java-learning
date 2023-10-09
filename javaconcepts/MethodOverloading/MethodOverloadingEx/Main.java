9302a606d135b928c9233546b5da33136660dc82
Vamsi Krishna <20816971+petlurivamsi@users.noreply.github.com>
2023-10-09 10:35:34 +0530

    overloaded methods with double added

--- javaconcepts/MethodOverloading/MethodOverloadingEx/Main.java
+++ javaconcepts/MethodOverloading/MethodOverloadingEx/Main.java
@@ -12,6 +12,15 @@ public class Main {
 
         System.out.println("The values are "+sum1+" "+sum2+" "+sum3);
 
+        double sumd1 = add(1.0,2.0);
+        System.out.println("Sum1 in double is "+sumd1);
+
+        double sumd2 = add(1.0,2.0,3.0);
+        System.out.println("Sum2 in double is "+sumd2);
+
+        double sumd3 = add(1.0,2.0,3.0,4.0);
+        System.out.println("Sum3 in double is "+sumd3);
+
     }
 
     static int add(int a,int b){
@@ -24,6 +33,18 @@ public class Main {
     }
     static int add(int a,int b,int c,int d){
         System.out.println("method#3 with four parameters ");
+        return a+b+c+d;
+    }
+     static double add(double a,double b){
+        System.out.println("method#1 in double with two parameters ");
+        return a+b;
+    }
+    static double add(double a,double b,double c){
+        System.out.println("method#2 in double with three parameters ");
+        return a+b+c;
+    }
+    static double add(double a,double b,double c,double d){
+        System.out.println("method#3 in double with four parameters ");
         return a+b+c+d;
     }
 
